5c1e560352a9a78ec7abff075b06e75a0a22abec
Данил Марков <markov.danil.04@mail.ru>
2025-03-21 09:13:45 +0300

    dev deploy with ngrok

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,4 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true, // Разрешает доступ извне
+    port: 5173, // Указываем порт
+    strictPort: true, // Гарантируем, что порт не изменится
+    allowedHosts: ['.ngrok-free.app'], // Разрешаем все ngrok-домены
+  },
 })
