f57016c5f970adaaf895b832745fae5bcba4ab0d
Markov Danil <markov.danil.04@mail.ru>
2025-03-21 14:19:11 +0300

    testing connection via ngrok

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,6 +3,12 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  app.enableCors({
+    origin: '*', // для тестов можно разрешить все домены
+    credentials: true,
+  });
+
+  await app.listen(4200);
 }
-bootstrap();
+bootstrap().then(() => console.log('server start on 4200'));
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,34 +1,37 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
+import { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [response, setResponse] = useState<any | null>(null);
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    fetch('https://f8bf-128-140-117-243.ngrok-free.app/hello/getHello', {
+      headers: {
+        'ngrok-skip-browser-warning': 'true', // любое значение
+      },
+    })
+      .then(res => {
+        if (!res.ok) {
+          throw new Error(`Ошибка: ${res.status} ${res.statusText}`);
+        }
+        return res.text();
+      })
+      .then(data => {
+        setResponse(JSON.parse(data));
+      })
+      .catch(err => {
+        setError(err.message);
+        setResponse(null); // Обнуляем response, если есть ошибка
+      });
+  }, []);
 
   return (
-    <>
-      <div>
-        <a href='https://vite.dev' target='_blank'>
-          <img src={viteLogo} className='logo' alt='Vite logo' />
-        </a>
-        <a href='https://react.dev' target='_blank'>
-          <img src={reactLogo} className='logo react' alt='React logo' />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className='card'>
-        <button onClick={() => setCount(count => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className='read-the-docs'>
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className='App'>
+      <h1>Ответ с сервера:</h1>
+      {error && <p style={{ color: 'red' }}>Ошибка: {error}</p>}
+      {response ? <p>{response.message}</p> : !error && <p>Загрузка...</p>}
+    </div>
   );
 }
 
